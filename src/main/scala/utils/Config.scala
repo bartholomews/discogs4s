ff93f2e90fccd906edfc51a3389e40bd891ba247
Federico Bartolomei <fbarto01@mail.bbk.ac.uk>
2018-07-22 22:51:09 +0100

    Added Config

--- src/main/scala/utils/Config.scala
+++ src/main/scala/utils/Config.scala
@@ -3,5 +3,7 @@ package utils
 import com.typesafe.config.ConfigFactory
 
 object Config {
-  val DISCOGS_API: String = ConfigFactory.load().getString("discogs.api")
+  private val factory = ConfigFactory.load()
+  val SCHEME: String = factory.getString("discogs.scheme")
+  val DISCOGS_API: String = factory.getString("discogs.api")
 }
