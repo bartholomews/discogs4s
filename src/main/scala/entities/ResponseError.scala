93bbd73c73c1710747360105c3343fe53b2ee3e8
Federico Bartolomei <fbarto01@mail.bbk.ac.uk>
2018-08-19 19:29:55 +0100

    Undo logging error

--- src/main/scala/entities/ResponseError.scala
+++ src/main/scala/entities/ResponseError.scala
@@ -15,8 +15,9 @@ object ResponseError extends Logger {
                                        override val getMessage: String) extends ResponseError
 
   def apply(throwable: Throwable, status: Status = Status.InternalServerError): ResponseError = {
-    logError(throwable) match {
-      case circeError: io.circe.Error => ResponseErrorImpl(Status.InternalServerError, circeError,
+    // TODO logError(throwable) match {
+    throwable match {
+    case circeError: io.circe.Error => ResponseErrorImpl(Status.InternalServerError, circeError,
         "There was a problem decoding or parsing this response, please check the error logs."
       )
       case _ => ResponseErrorImpl(status, throwable, throwable.getMessage)
