c37b9b72e9b00c3957d4e19ab6c730eb50cbe27b
bartholomews <git@bartholomews.io>
2020-04-12 13:47:48 +0100

    Removed unnecessary deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,13 +1,5 @@
 import sbt._
 
-object Versions {
-  // https://github.com/scalatest/scalatest/releases
-  val scalaTest = "3.1.1"
-  //
-
-  val wiremock = "2.25.0"
-}
-
 object Dependencies {
 
   val dependencies: Seq[ModuleID] = Seq(
@@ -18,15 +10,7 @@ object Dependencies {
   )
 
   lazy val testDependencies: Seq[ModuleID] = Seq(
-    "io.bartholomews" %% "scalatestudo" % "0.0.1-SNAPSHOT",
-    // https://mvnrepository.com/artifact/org.slf4j/slf4j-log4j12
-    // libraryDependencies += "org.slf4j" % "slf4j-log4j12" % "1.7.25" % Test
-    "org.scalactic" %% "scalactic" % Versions.scalaTest,
-    // http://www.scalatest.org/user_guide/using_scalatest_with_sbt
-    "org.scalatest" %% "scalatest" % Versions.scalaTest,
-    "com.github.tomakehurst" % "wiremock" % Versions.wiremock,
-    // https://github.com/softwaremill/diffx/releases
-    "com.softwaremill.diffx" %% "diffx-scalatest" % "0.3.12"
+    "io.bartholomews" %% "scalatestudo" % "0.0.1-SNAPSHOT"
   ).map(_ % Test)
 
 }
