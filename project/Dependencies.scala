071a13deb6830d30bf2ab98cfb00cf17c0a97dd7
bartholomews <git@bartholomews.io>
2020-05-23 16:00:26 +0100

    fsclient v0.0.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  private val fsClientVersion = "0.1.0-SNAPSHOT"
+  private val fsClientVersion = "0.0.2"
 
   val dependencies: Seq[ModuleID] = Seq(
     "io.bartholomews" %% "fsclient" % fsClientVersion
