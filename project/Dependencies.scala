ae2ab5388ea281cac782b4e8190e66e01628abb2
bartholomews <github@bartholomews.io>
2021-01-03 15:56:18 +0000

    snapshot deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies {
 
-  private val fsClientVersion = "0.1.0+0-2df98ab4+20210102-1839-SNAPSHOT"
+  private val fsClientVersion = "0.1.0+1-08b48bdf-SNAPSHOT"
 
   val dependencies: Seq[ModuleID] = Seq(
     "io.bartholomews" %% "fsclient-circe" % fsClientVersion
   )
 
   val testDependencies: Seq[ModuleID] = Seq(
-    "io.bartholomews" %% "scalatestudo" % "0.0.3+1-439ed110+20210102-1855-SNAPSHOT"
+    "io.bartholomews" %% "scalatestudo" % "0.0.3+2-4ad35c91-SNAPSHOT"
   ).map(_ % Test)
 }
